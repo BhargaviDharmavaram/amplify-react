5b78d0a87f18c7b588a3152f26525dc39d59d787
BhargaviDharmavaram <bhargavidharmavaram2001@gmail.com>
2024-07-16 12:20:56 +0530

    added api and auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Todo from "./Todo";
 import { Amplify } from 'aws-amplify';
+import { generateClient } from 'aws-amplify/api';
+import { Authenticator } from '@aws-amplify/ui-react';
 
 import {
   withAuthenticator,
@@ -12,16 +14,41 @@ import '@aws-amplify/ui-react/styles.css';
 import config from './amplifyconfiguration.json';
 Amplify.configure(config);
 
+const client = generateClient();
 
-function App({ signOut }) {
+// async function fetchTodos() {
+//   try {
+//     const {data} = await client.graphql({query: listTodos}, { authMode: 'AMAZON_COGNITO_USER_POOLS' });
+//     console.log('Todos:', data.listTodos.items);
+//     return data.listTodos.items;
+//   } catch (err) {
+//     console.log('Error fetching todos', err);
+//   }
+// }
+// function App({ signOut }) {
+//   //fetchTodos();
+//   return (
+//     <View className="App">
+//       <Card>
+//         <Todo />
+//       </Card>
+//       <Button onClick={signOut}>Sign Out</Button>
+//     </View>
+//   );
+// }
+
+// export default withAuthenticator(App);
+
+export default function App() {
   return (
-    <View className="App">
-      <Card>
-        <Todo />
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <Todo/>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
-}
-
-export default withAuthenticator(App);
+};
